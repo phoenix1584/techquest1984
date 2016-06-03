693a75fb0d82d20b497dfe01133de3350c08fd55
subodh <subodh.austin@gmail.com>
2016-06-03 16:59:06 +0530

    Adding directories for Algos_DataStructs,BuildTools,Boost,Concurrency,Crypto

---
+++ BuildTools/include/base.hh
@@ -0,0 +1,44 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2016 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*******************************************************************************/
+#ifndef BASE_HH
+#define BASE_HH
+
+#endif /* BASE_HH */
+
+#include <stdint.h>
+
+class BaseMessage{
+    private:
+        int8_t m_char;
+        int16_t m_short;
+        int32_t m_int;
+        int64_t m_long;
+        float m_float;
+        double m_double;
+
+    public:
+
+         int32_t GetInt() const;
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # techquest1984
 Companion repository for blog and other demo codes
+.
+├── Algos_DataStructs - Algorithms and data strucutures
+│   ├── bin
+│   ├── include
+│   ├── lib
+│   ├── src
+│   └── test
+├── Boost - Experiments with Boost and C++98
+│   ├── bin
+│   ├── include
+│   ├── lib
+│   ├── src
+│   └── test
+├── BuildTools - Experiments with build tools like Make, libtool,cmake etc.
+│   ├── bin
+│   ├── include
+│   ├── lib
+│   ├── src
+│   └── test
+├── Concurrency - Threads and networking examples
+│   ├── bin
+│   ├── include
+│   ├── lib
+│   ├── src
+│   └── test
+├── Crypto - Expermients with crpytography libraries
+│   ├── bin
+│   ├── include
+│   ├── lib
+│   ├── src
+│   └── test
+├── DLang - D Language experiments
+└── GO - Go Language experiments
