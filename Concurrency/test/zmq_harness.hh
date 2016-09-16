2bed441b802583d47cd4d315b137f1a78338daff
subodh <subodh.austin@gmail.com>
2016-09-16 18:28:45 +0530

    [BROKEN] updated the harness helper classes. Desgin a command que submission logic for refined testing.

--- Concurrency/test/zmq_harness.hh
+++ Concurrency/test/zmq_harness.hh
@@ -3,6 +3,7 @@
 
 #include <boost/assign.hpp>
 #include <set>
+#include <deque>
 namespace utils{
     class TestUtils{
         private:
@@ -10,12 +11,8 @@ namespace utils{
             std::string m_default_response;
         public:
             TestUtils()
-                :m_default_response("UNKOWN_COMMAND")
-            {
-                m_cmd_resp_dict = boost::assign::map_list_of("REGISTER", "REG_ACK")
-                    ("COMMAND_1", "CMD_ACK")
-                    ("TASK_ACK", "TASK_COMPLETED")
-                    ("EXIT_ACK","EXIT");
+                :m_default_response("UNKOWN_COMMAND"){
+                m_cmd_resp_dict = boost::assign::map_list_of("A_REGISTER","REG_ACK")("B_COMMAND_1","CMD_ACK")("C_TASK","TASK_ACK")("X_STOP","STOP_ACK");
             }
             std::string LogPathCreator(std::string test_name){
                 std::string log_path("/tmp/");
@@ -38,21 +35,31 @@ namespace utils{
                  return (it != m_cmd_resp_dict.end()) ? it->second : m_default_response ;
                 
             }
+
+            const std::map<std::string,std::string> GetDictionary(){
+                return m_cmd_resp_dict;
+            }
     };
 
     class client_task {
         private:
             bool MessageHandler()
             {   
-                std::stringstream data ;
+                std::deque<std::string> data;
                 int more = 0;
                 size_t more_size = sizeof(more);
                 do{
-                    data << std::string(s_recv(client_socket_));
+                    data.push_back(s_recv(client_socket_));
                     client_socket_.getsockopt(ZMQ_RCVMORE, &more, &more_size);
                 }while(more);
-                BOOST_TEST_MESSAGE(data.str());
-                return (strcmp(data.str().c_str(),"EXIT"));
+                if(std::strcmp(data[0].c_str(),m_test_utils.GetResponse("X_STOP").c_str())){
+                    BOOST_TEST_MESSAGE("sending command .. X_STOP"  );
+                    client_socket_.send("X_STOP",6);
+                    return true;
+                }
+                else{
+                    return false;
+                }
             }
         public:
             client_task()
@@ -62,10 +69,11 @@ namespace utils{
         {}
             void run() {
                 s_catch_signals();
+                int linger = 0;
                 client_socket_.setsockopt(ZMQ_IDENTITY, m_identity.c_str(), m_identity.length());
+                client_socket_.setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
                 client_socket_.connect("tcp://localhost:5570");
-
-                s_send(client_socket_,"REGISTER");
+                s_send(client_socket_,"A_REGISTER");
                 zmq::pollitem_t items[] = {{static_cast<void *>(client_socket_), 0, ZMQ_POLLIN, 0}};
                 try {
                     while(!s_interrupted){
@@ -84,6 +92,7 @@ namespace utils{
             zmq::context_t ctx_;
             zmq::socket_t client_socket_;
             const std::string m_identity;
+            std::deque<std::string> m_test_queue; 
 
     };
 
@@ -97,20 +106,25 @@ namespace utils{
                     data.push_back(s_recv(frontend_));
                     frontend_.getsockopt(ZMQ_RCVMORE, &more, &more_size);
                 }while(more);
+                BOOST_TEST_MESSAGE(data[1]);
                 frontend_.send(data[0].c_str(),data[0].length(),ZMQ_SNDMORE);
-                s_send(frontend_,"EXIT");
-                return false;
+                s_send(frontend_,m_test_utils.GetResponse(data[1]));
+                return (std::strcmp(data[1].c_str(),"X_STOP"));
             }
         public:
             server_task()
-                : ctx_(1),
-                frontend_(ctx_, ZMQ_ROUTER),
-                m_identity("SERVER")
+                :m_cmd_itr(m_test_utils.GetDictionary().begin())
+                ,ctx_(1)
+                ,frontend_(ctx_, ZMQ_ROUTER)
+                ,m_identity("SERVER")
         {}
 
             void run() {
                 s_catch_signals();
                 frontend_.bind("tcp://*:5570");
+                int linger = 0;
+                frontend_.setsockopt(ZMQ_IDENTITY, m_identity.c_str(), m_identity.length());
+                frontend_.setsockopt(ZMQ_LINGER, &linger, sizeof(linger));
                 zmq::pollitem_t items[] = {{static_cast<void *>(frontend_), 0, ZMQ_POLLIN, 0}};
                 try {
                     while(!s_interrupted){
