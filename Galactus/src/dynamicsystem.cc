44d9163827f3562f0692911b79cc18b86fb9a7fc
subodh <subodh.austin@gmail.com>
2017-06-23 22:26:59 +0530

    Dynamic system implementation and test cases.

--- Galactus/src/dynamicsystem.cc
+++ Galactus/src/dynamicsystem.cc
@@ -1,1 +1,32 @@
 #include "dynamicsystem.hh"
+using namespace galaxy_trade;
+
+void DynamicNumberSystem::AddSymbol(std::string dyn_symbol,std::string roman_symbol){
+    if(IsRoman(roman_symbol) && dyn_symbol.size()){
+       m_dyn_dictionary.insert(std::pair<std::string,std::string>(dyn_symbol,roman_symbol)); 
+    }else{
+        throw DynamicDataException("Invalid data submitted for addition to dynamic numer system. Empty symbol or invalid roman symbol.");
+    }
+}
+
+bool DynamicNumberSystem::IsValidSymbol(const std::string s){
+    return (m_dyn_dictionary.end() != m_dyn_dictionary.find(s));
+}
+
+unsigned int DynamicNumberSystem::ToValue(std::string sym){
+    std::map<std::string,std::string>::iterator itr = m_dyn_dictionary.find(sym);
+    if(itr != m_dyn_dictionary.end()){
+        return RomanSystem::ToValue(itr->second);
+    }else{
+        throw DynamicDataException("Unkown dynamic symbol.");
+    }
+}
+
+std::string DynamicNumberSystem::ToRoman(std::string sym){
+    std::map<std::string,std::string>::iterator itr = m_dyn_dictionary.find(sym);
+    if(itr != m_dyn_dictionary.end()){
+        return itr->second;
+    }else{
+        throw DynamicDataException("Unkown dynamic symbol.");
+    }
+}
---
+++ Galactus/test/t_dynamic_system.cc
@@ -0,0 +1,40 @@
+/*
+* Objective
+* Test cases for dynamic numeric implementation
+*/
+
+#define PF_BTM BOOST_TEST_MESSAGE(__PRETTY_FUNCTION__)
+#define BT_START BOOST_TEST_MESSAGE( "\n**************************************************************************\n" \
+        << boost::unit_test::framework::current_test_case().p_name << \
+        "\n**************************************************************************\n")
+
+#define BOOST_TEST_MODULE ROMAN_SYSTEM 
+#include <boost/test/unit_test.hpp>
+#include "dynamicsystem.hh"
+
+using namespace galaxy_trade;
+
+/// Could add more symbols if needed but enough to prove that file could be used to perform similar task.
+void Populate(DynamicNumberSystem& data){
+    data.AddSymbol("glob","I");
+    data.AddSymbol("prok","V");
+    data.AddSymbol("pish","X");
+    data.AddSymbol("tegj","L");
+}
+
+BOOST_AUTO_TEST_SUITE(roman_system)
+    BOOST_AUTO_TEST_CASE(add_value){
+        BT_START;
+        DynamicNumberSystem dns;
+        Populate(dns);
+        BOOST_TEST_MESSAGE(dns);
+    }
+
+BOOST_AUTO_TEST_CASE(get_value){
+    BT_START;
+    DynamicNumberSystem dns;
+    Populate(dns);
+    std::string test("prok");
+    BOOST_TEST_MESSAGE(" Test Input : prok => Roman : "<< dns.ToRoman("prok") << ", Value : " << dns.ToValue("prok"));
+}
+BOOST_AUTO_TEST_SUITE_END()
