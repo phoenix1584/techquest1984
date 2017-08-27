e892f845248e55bbb1cb7ce566195e18ae0fc001
phoenix1584 <phoenix1584@users.noreply.github.com>
2017-08-27 12:30:48 +0530

    Update MergeSort.cc
    update routine work in progress

--- AlgosDataStructs/src/MergeSort.cc
+++ AlgosDataStructs/src/MergeSort.cc
@@ -3,13 +3,30 @@
 #include <vector>
 
 void Merge(std::vector<int>& sub_data,unsigned int left_index, unsigned int middle_index, unsigned int right_index){
-       const int l_size = middle_index - left_index + 1;
+       std::cout << "l_index : " << left_index << ", Right_index : " << right_index << ",Middle_index : " << middle_index;
+       const int l_size = middle_index - left_index ;
        const int r_size = right_index - middle_index;
-       std::vector<int> l_data(l_size);
-       std::vector<int> r_data(r_size);
-       std::copy(sub_data.begin()+left_index,sub_data.begin()+left_index+l_size,l_data.begin());
-       std::copy(sub_data.begin()+middle_index+1,sub_data.begin()+middle_index + 1 + r_size,l_data.begin());
+       std::vector<int> l_data,r_data;
+       std::vector<int>::iterator itr = sub_data.begin()+left_index;
+       for(int x = 0; x < l_size ; ++x){
+            l_data.push_back(*itr); 
+            ++itr;
+        }
+       itr = sub_data.begin()+ middle_index + 1;
+       for(int x = 0; x < r_size ; ++x){
+            r_data.push_back(*itr); 
+            ++itr;
+        }
+         
+       std::cout << " Left Data : ";
+       for(const auto& x : l_data)
+            std::cout << x << ",";
+       std::cout << " Right Data : ";
+       for(const auto& x : r_data)
+            std::cout << x << ",";
+        std::cout << "\n";
        
+
        // Merging the data
        int i = 0, j =0 , k = left_index;
        while(i < l_size && j < r_size){
@@ -18,6 +35,7 @@ void Merge(std::vector<int>& sub_data,unsigned int left_index, unsigned int midd
                 i++;
             }else{
                 sub_data[k] = r_data[j];
+                j++;
             }
             k++;
         }
@@ -25,21 +43,22 @@ void Merge(std::vector<int>& sub_data,unsigned int left_index, unsigned int midd
 
 void MSort(std::vector<int>& sub_data,unsigned int left_index, unsigned int right_index){
     if(left_index < right_index){
-        int middle_index = (left_index + right_index)/2;
+        int middle_index = left_index + (right_index-left_index) /2;
         MSort(sub_data,left_index,middle_index);
         MSort(sub_data,middle_index+1,right_index);
-        Merge(sub_data,left_index,middle_index,right_index);
+        Merge(sub_data,left_index,middle_index + 1,right_index);
     }
     
 };
 
 int main()
 {
-    std::vector<int> data = {10,5,6,1,4};
+    std::vector<int> data = {38,27,43,3,9,82,10};
     for(const auto& x : data)
         std::cout << x << ",";
     std::cout << "\nStarting the sort ...\n";
     MSort(data,0,data.size());
     for(const auto& x : data)
-        std::cout << x << ",";
+        std::cout << x << ",";  
+    std::cout<< "\nFinished" << std::endl;
 }
